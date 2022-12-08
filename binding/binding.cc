c5e926d64b86869ba4997e4b101ea80e448ca7c4
altinak <altinak@zero.gg>
2022-12-08 14:15:51 -0500

    update

--- binding/binding.cc
+++ binding/binding.cc
@@ -11,30 +11,38 @@ public:
   MinisketchWrapper(int field_size_, int implementation_number_, int capacity_) : field_size(field_size_), implementation_number(implementation_number_), capacity(capacity_) {
     sketch = minisketch_create(field_size_, implementation_number_, capacity_);
   }
-  int getFieldSize() { return field_size; }
-  int getImplementationNumber() { return implementation_number; }
-  int getCapacity() { return capacity; }
-  void setFieldSize(int field_size_) { field_size = field_size_; }
-  void setCapacity(int capacity_) { capacity = capacity_; }
-  void setImplementationNumber(int implementation_number_) { implementation_number = implementation_number_; }
-  void destroy() {
+  int GetFieldSize() { return field_size; }
+  int GetImplementationNumber() { return implementation_number; }
+  int GetCapacity() { return capacity; }
+  void SetFieldSize(int field_size_) { field_size = field_size_; }
+  void SetCapacity(int capacity_) { capacity = capacity_; }
+  void SetImplementationNumber(int implementation_number_) { implementation_number = implementation_number_; }
+  void Destroy() {
     minisketch_destroy(sketch);
   }
-  val serialize() {
+  val Serialize() {
     int len = minisketch_serialized_size(sketch);
     last_serialized = (unsigned char *) malloc(len);
     minisketch_serialize(sketch, last_serialized);
     return val(typed_memory_view(len, last_serialized));
   }
-  void destroySerialized() {
+  void DestroySerialized() {
     if (last_serialized) free(last_serialized);
     last_serialized = (unsigned char *) 0;
   }
-  void addUint(unsigned long i) {
+  void AddUint(unsigned long i) {
      minisketch_add_uint64(sketch, i);
   }
-  void deserialize(unsigned char *serialized) {
-    minisketch_deserialize(sketch, serialized);
+  void Deserialize(std::string serialized) {
+    minisketch_deserialize(sketch, serialized.to_cstr());
+  }
+  void Merge(MinisketchWrapper *other_sketch) {
+    minisketch_merge(sketch, other_sketch->sketch);
+  }
+  std::vector<unsigned long> decode(unsigned int max_elements) {
+    unsigned long *buffer = new unsigned long[](max_elements);
+    minisketch_decode(sketch, max_elements, (uint64_t *) buffer);
+    return std::vector<unsigned long>((unsigned long *) buffer);
   }
 private:
   int field_size;
@@ -47,11 +55,8 @@ private:
 EMSCRIPTEN_BINDINGS(MinisketchWrapper) {
   class_<MinisketchWrapper>("MinisketchWrapper")
     .constructor<int, int, int>()
-    .function("addUint", &MinisketchWrapper::addUint)
-    .function("serialize", &MinisketchWrapper::serialize)
-    .function("destroySerialized", &MinisketchWrapper::destroySerialized);
+    .function("addUint", &MinisketchWrapper::AddUint)
+    .function("serialize", &MinisketchWrapper::Serialize)
+    .function("destroySerialized", &MinisketchWrapper::DestroySerialized);
+    .function("deserialize", &MinisketchWrapper::Deserialize)
 }
-
-//  .function("deserialize", &MinisketchWrapper::deserialize)
-// .property("implementation_number", &MinisketchWrapper::getImplementationNumber, &MinisketchWrapper::setImplementationNumber)
-//  .property("capacity", &MinisketchWrapper::getCapacity, &MinisketchWrapper::setCapacity)
